c2ccb1990e2bebb4d562ad85e811fb086ebc0608
Takashi Furuhashi <tf@yorozu-sys.net>
2020-05-17 21:27:58 +0900

    OS判断を追加

--- install.sh
+++ install.sh
@@ -1,5 +1,20 @@
-#!/bin/sh
+#!/bin/bash
+# if exists setting file then overwritting.
+# 2020/05/15
+# Takashi Furuhashi <tf@yorozu-sys.net>
 
+# OS research
+OS=""
+if [ "$(uname -s)" == 'Darwin' ]; then
+	OS='Mac'
+elif [ "$(uname -s)" == 'Linux' ]; then
+	OS='Linux'
+elif [ "$(uname -s)" == 'MINGW32_NT' ]; then                                                                                           
+	OS='Cygwin'
+else
+	echo "Your platform ($(uname -a)) is not supported."
+	exit 1
+fi
 echo -n "install for dotfiles..."
 
 cd $HOME/dotfiles
