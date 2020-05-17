14b4308cf0ed88ea75bf4eff7231cf78e116b423
Takashi Furuhashi <tf@yorozu-sys.net>
2020-05-17 21:28:25 +0900

    インストール処理をもう少しちゃんとした。

--- install.sh
+++ install.sh
@@ -15,9 +15,48 @@ else
 	echo "Your platform ($(uname -a)) is not supported."
 	exit 1
 fi
+
+DORFILESDIR="$HOME/dotfiles"
+
 echo -n "install for dotfiles..."
 
 cd $HOME/dotfiles
-ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+
+# install .gitconfig
+if [ -f $HOME/.gitconfig ]
+then
+	rm -f $HOME/.gitconfig
+fi
+ln -s $DORFILESDIR/git/.gitconfig $HOME/.gitconfig
+
+#install Visual Studio Code settings
+if [ $OS == 'Mac' ]
+then
+	cd $HOME/Library/"Application Support"/Code/User/
+
+	# settings.json
+	if [ -f settings.json ]
+	then
+		rm -f settings.json
+	fi
+	ln -s $DORFILESDIR/vscode/settings.json .
+
+	# keybindings.json
+	if [ -f keybindings.json ]
+	then
+		rm -f keybindings.json
+	fi
+	ln -s $DORFILESDIR/vscode/keybindings.json .
+
+	# snippets directory
+	if [ -d snippets ]
+	then
+		rm -rf snippets
+	fi
+	ln -s $DORFILESDIR/vscode/snippets .
+
+fi
+
+cd $HOME
 
 echo "done."
