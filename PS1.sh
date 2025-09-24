a0499c70910d64d79e7d25dae7ac3bbe793bd929
Takashi Furuhashi <takashi.furuhashi@globalway.co.jp>
2025-09-24 12:38:02 +0900

    updte.

--- PS1.sh
+++ PS1.sh
@@ -10,11 +10,16 @@ tmux_session_name() {
   [ -n "$TMUX" ] && echo "TMUX:$(tmux display-message -p '#S')"
 }
 
+# バックグラウンドジョブ数
+jobs_count() {
+  echo "JOBS:$(jobs | wc -l)"
+}
+
 # プロンプト設定
 export PS1="\n[\[\033[01;33m\]\$(date '+%Y-%m-%d %H:%M:%S') \
-\[\033[01;36m\]HIST:\! \
+\[\033[01;36m\]\$(jobs_count) \
+HIST:\! \
 \[\033[01;35m\]\$(parse_git_branch) \
 \[\033[01;32m\]\$(tmux_session_name)\[\033[00m\]]\n\
-\[\]\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\
 \[\033[01;32m\]\u@\h\[\033[00m\]:\
-\[\033[01;34m\]\w\[\033[00m\]\$ \[\]"
+\[\033[01;34m\]\w\[\033[00m\]\$ "
